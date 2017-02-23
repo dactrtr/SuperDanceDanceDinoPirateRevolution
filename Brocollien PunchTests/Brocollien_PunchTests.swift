468f8ae4ea6b0e8474774713028de72c0c4c3c2e
dactrtr <dactrtr@gmail.com>
2017-02-23 12:47:55 -0300

    Initial Commit

---
+++ Brocollien PunchTests/Brocollien_PunchTests.swift
@@ -0,0 +1,36 @@
+//
+//  Brocollien_PunchTests.swift
+//  Brocollien PunchTests
+//
+//  Created by dactrtr z on 2/23/17.
+//  Copyright © 2017 dactrtr z. All rights reserved.
+//
+
+import XCTest
+@testable import Brocollien_Punch
+
+class Brocollien_PunchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
